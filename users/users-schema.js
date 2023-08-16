938b4dffa06c3641cd91474b5d45618038379acf
Larson Ost <larsonost@outlook.com>
2023-08-16 18:39:47 -0400

    creating embedded variables

--- users/users-schema.js
+++ users/users-schema.js
@@ -1,8 +1,22 @@
 import mongoose from "mongoose";
+const productSchema = new mongoose.Schema({
+  name: { type: String},
+  price: { type: String}
+});
+
+const dogSchema = new mongoose.Schema({
+  name: { type: String},
+  breed: { type: String}
+});
+
 const usersSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   firstName: String,
   lastName: String,
+  role: String,
+  dogs: { type: [dogSchema], default: [] },
+  service: String,
+  products: { type: [productSchema], default: [] }
 }, { collection: "users" });
 export default usersSchema;
